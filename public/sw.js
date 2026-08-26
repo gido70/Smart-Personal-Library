ff09ea767f24672757feac33a338e3d8ea780c8a
gido70 <63196885+gido70@users.noreply.github.com>
2026-08-26 15:29:41 +0400

    V0.7.1: update public/sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "smart-personal-library-v0.6.3";
+const CACHE_NAME = "smart-personal-library-v0.7.1";
 const APP_SHELL = ["./", "./manifest.webmanifest", "./favicon.svg"];
 
 self.addEventListener("install", (event) => {
