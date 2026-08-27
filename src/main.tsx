d16c3e97d5db1ed20fb8810fe23904a9df51fdf0
gido70 <63196885+gido70@users.noreply.github.com>
2026-08-27 11:53:50 +0400

    V0.11: make book journey pilot the preview app

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,14 @@ import "./lib/polyfills";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import AppV11 from "./AppV11";
 import "./globals.css";
 import "./reader.css";
 import "./pilot.css";
+import "./v011.css";
+
+const legacy = new URLSearchParams(window.location.search).get("legacy") === "1";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode><App /></React.StrictMode>,
+  <React.StrictMode>{legacy ? <App /> : <AppV11 />}</React.StrictMode>,
 );
